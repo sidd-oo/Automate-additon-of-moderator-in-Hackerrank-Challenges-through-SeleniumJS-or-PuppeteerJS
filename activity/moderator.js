4734bbeca5ac8a92e17442e432e6dddcd7739c29
sidd_oo <sahoosiddharth3@gmail.com>
2020-04-08 02:18:35 +0530

    Reading the cfile content through async await

--- activity/moderator.js
+++ activity/moderator.js
@@ -6,4 +6,13 @@ let driver = builder.forBrowser('chrome').build();
 
 let cfile = process.argv[2];
 let userToAdd = process.argv[3];
+ 
 
+(async function(){
+    //Reading the cfile content through async await
+    let contents = await fs.promises.readFile(cfile,"utf-8");
+    let credentials = JSON.parse(content);
+    let user = credentials.user;
+    let pwd = credentials.pwd;
+    let url = credentials.url;
+})();
