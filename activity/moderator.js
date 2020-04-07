fe8048e4dc081b550984885964a9ae94bcd02a81
sidd_oo <sahoosiddharth3@gmail.com>
2020-04-08 03:44:47 +0530

    Extraction of admin href and impementation of ImplicitWait

--- activity/moderator.js
+++ activity/moderator.js
@@ -7,27 +7,47 @@ let driver = builder.forBrowser('chrome').build();
 let cfile = process.argv[2];
 let userToAdd = process.argv[3];
 
-(async function(){
-    //Reading the cfile content through async await
-    let contents = await fs.promises.readFile(cfile,"utf-8");
-    let credentials = JSON.parse(contents);
-    let user = credentials.user;
-    let pwd = credentials.pwd;
-    let url = credentials.url;
+(async function () {
+    try {
 
-    //Opening the Hackerrank's url
-    await driver.get(url);
-    let usernameElement = await driver.findElement(sd.By.css('#input-1'));
-    let passwordElement = await driver.findElement(sd.By.css('#input-2'));
-    
-    //Passing of keys into username and password text fields
-    await usernameElement.sendKeys(user);
-    await passwordElement.sendKeys(pwd);
-    
-    //Trying to hit login button
-    let loginButtonElement = await driver.findElement(sd.By.css('.auth-button'));
-    await loginButtonElement.click();
+        //This prevents from getting immediate error. It waits till the whole pages loads.
+        //It identify performance issues and finds when a webpage takes too much time to load and catch it in your test.
+        //ImplicitWait ::: Using Implicit Wait, we can tell WebDriver to wait for a certain amount of time when trying to
+        //element(s) on the page.Any search for elements on the page could take the time the implicit wait is set for 
+        //i.e.for the entire time the browser is open.The default value is 0. We can use Implicit Wait as follows:
 
-    
+        await driver.manage().setTimeouts({
+            implicit: 10000,
+            pageLoad: 10000
+        });
 
+        //Reading the cfile content through async await
+        let contents = await fs.promises.readFile(cfile, "utf-8");
+        let credentials = JSON.parse(contents);
+        let user = credentials.user;
+        let pwd = credentials.pwd;
+        let url = credentials.url;
+
+        //Opening the Hackerrank's url
+        await driver.get(url);
+        let usernameElement = await driver.findElement(sd.By.css('#input-1'));
+        let passwordElement = await driver.findElement(sd.By.css('#input-2'));
+
+        //Passing of keys into username and password text fields
+        await usernameElement.sendKeys(user);
+        await passwordElement.sendKeys(pwd);
+
+        //Trying to hit login button
+        let loginButtonElement = await driver.findElement(sd.By.css('.auth-button'));
+        await loginButtonElement.click();
+
+        //Extracting the href of Admistration page from top-right dropdown menu
+        let admistrationElement = (await driver).findElement(sd.By.css("a[data-analytics=NavBarProfileDropDownAdministration]"));
+        let admistrationElementurl = await admistrationElement.getAttribute("href");
+        await driver.get(admistrationElementurl);
+
+
+    } catch (err) {
+        console.log(err);
+    }
 })();
