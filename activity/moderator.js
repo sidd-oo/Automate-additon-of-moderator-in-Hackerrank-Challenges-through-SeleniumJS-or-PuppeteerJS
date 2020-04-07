31c96e077389cbadbfdc28af5cf7a6a8144de311
sidd_oo <sahoosiddharth3@gmail.com>
2020-04-08 02:41:08 +0530

    Opening the url and login credentials are passed

--- activity/moderator.js
+++ activity/moderator.js
@@ -6,13 +6,24 @@ let driver = builder.forBrowser('chrome').build();
 
 let cfile = process.argv[2];
 let userToAdd = process.argv[3];
- 
 
 (async function(){
     //Reading the cfile content through async await
     let contents = await fs.promises.readFile(cfile,"utf-8");
-    let credentials = JSON.parse(content);
+    let credentials = JSON.parse(contents);
     let user = credentials.user;
     let pwd = credentials.pwd;
     let url = credentials.url;
+
+    console.log(url);
+    //Opening the Hackerrank's url
+    await driver.get(url);
+    let usernameElement = await driver.findElement(sd.By.css('#input-1'));
+    let passwordElement = await driver.findElement(sd.By.css('#input-2'));
+    //Passing of keys into username and password text fields
+    await usernameElement.sendKeys(user);
+    await passwordElement.sendKeys(pwd);
+
+    
+
 })();
